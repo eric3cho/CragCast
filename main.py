fd6c804528e55198a5be03a97f8566ebe03255dc
eric_cho <eric3cho@gmail.com>
2025-07-18 00:04:58 -0700

    weather api

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# flow: get date > check area temps > determine if good conditions > display results
 from datetime import date
 
+# flow: get date > check area temps > determine if good conditions > display results
 today = date.today()
 print("Today is " + str(today) + ". I can CragCast up to 16 days in advance.")
 days = input("What days are you climbing? Put in form 'start,end' (i.e. 11,13): ")
---
+++ weather.py
@@ -0,0 +1,7 @@
+# open-meteo API
+import openmeteo_requests
+import pandas as pd
+import requests _cache
+from retry_requests import retry
+
+cache_session
