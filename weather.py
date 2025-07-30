96d01dd2866dcfc1df527d377df5d48e27fc5bcb
eric_cho <eric3cho@gmail.com>
2025-07-30 15:43:20 -0700

    test api req

--- weather.py
+++ weather.py
@@ -19,7 +19,7 @@ def forecast():
     url = "https://api.open-meteo.com/v1/forecast"
     params = {
         "latitude": 47.84769,
-        "longitude": 121.60991,
+        "longitude": -121.60991,
         "hourly": [
             "temperature_2m",
             "wind_speed_10m",
@@ -28,11 +28,26 @@ def forecast():
             "relative_humidity_2m",
             "weathercode"
         ],
-        "forecast_days": 16,
+        "forecast_days": 3,
         "timezone": "auto"
     }
     responses = openmeteo.weather_api(url, params=params)
-    print(responses)
+
+    # process hourly data
+    response = responses[0]
+    hourly = response.Hourly()
+    hourly_data = {
+        # "time": hourly.Time(),
+        "temperature_2m": hourly.Variables(0).ValuesAsNumpy(),  # temperature
+        "wind_speed_10m": hourly.Variables(1).ValuesAsNumpy(),
+        "precipitation": hourly.Variables(2).ValuesAsNumpy(),
+        "cloudcover": hourly.Variables(3).ValuesAsNumpy(),
+        "relative_humidity_2m": hourly.Variables(4).ValuesAsNumpy(),
+        "weathercode": hourly.Variables(5).ValuesAsNumpy()
+    }
+
+    df = pd.DataFrame(data=hourly_data)
+    print(df.head())
 
 
 
