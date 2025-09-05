3e10f73cd19a3d59969f48e41b5d3a8576c04451
eric3cho <eric3cho@uw.edu>
2025-09-04 20:51:43 -0700

    requests

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 from flask import Flask, render_template
+import openmeteo_requests
+import requests_cache
+from retry_requests import retry
 from locations import locations
 
 app = Flask(__name__)
 
+cache_session = requests_cache.CachedSession(".cache", expire_after=3600)
+retry_session = retry(cache_session, retries=3, backoff_factor=0.2)
+openmeteo = openmeteo_requests.Client(session=retry_session)
 
 def get_forecast(lat, lon):
     url = "https://api.open-meteo.com/v1/forecast"
     params = {
         "latitude": lat,
         "longitude": lon,
-        "daily": "temperature_2m_max,temperature_2m_min,precipitation_sum,wind_speed_10m_max,relative_humidity_2m_max",
+        "daily": ["temperature_2m_max",temperature_2m_min,precipitation_sum,wind_speed_10m_max,relative_humidity_2m_max",
         "hourly": "temperature_2m,wind_speed_10m,precipitation,cloudcover,relative_humidity_2m",
         "timezone": "auto"
     }
-    # response = requests.get(url, params=params)
+    response = requests.get(url, params=params)
 
 
 @app.route("/")
